1f10828cf2bb8f619a7a39c933afdae21626148d
Kartikey Sharma <kartikeysharma1616@gmail.com>
2022-10-28 22:54:35 +0530

    Update and rename 2236. Root Equals Sum of Children.txt to 2236. Root Equals Sum of Children.java

--- 2236. Root Equals Sum of Children.java
+++ 2236. Root Equals Sum of Children.java
@@ -1,6 +1,5 @@
 class Solution {
-    public boolean checkTree(TreeNode root) 
-    {
+    public boolean checkTree(TreeNode root){
         if((root.left.val+ root.right.val)== root.val)   
             return true;
         else 
